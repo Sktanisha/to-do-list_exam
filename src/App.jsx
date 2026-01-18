9cebaaca30bafb9b0b1ea9dd37e0fae8c3d627b3
Sktanisha <sanjidatanisha99@gmail.com>
2026-01-18 01:12:16 -0500

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ const App = () => {
 
   return (
     
-  <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-200 flex items-center justify-center p-4">
+  <div className="min-h-screen bg-linear-to-br from-slate-50 to-slate-200 flex items-center justify-center p-4">
     <div className="w-full max-w-xl">
       {/* Header */}
       <div className="text-center mb-6">
